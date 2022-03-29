c65b3030f4c091b92ecfcac3c68e5d01dfdc42f7
lsilvaV <91691219+lsilvaV@users.noreply.github.com>
2022-03-29 16:36:41 -0300

    Update Constants.php

--- model/Constants.php
+++ model/Constants.php
@@ -1,8 +1,7 @@
 <?php
-
-define('DB_HOST','silvioflorentino.mysql.database.azure.com');
-define('DB_USER','silvioflorentino@silvioflorentino');
-define('DB_PASS','1978@|Silvio');
-define('DB_NAME','hortfruitbd');
+    define('DB_HOST','leonardosv.mysql.database.azure.com');
+    define('DB_USER','usuario@leonardosv');
+    define('DB_PASS','P@ssw0rd');
+    define('DB_NAME','hortfruitbd');
 
 ?>
