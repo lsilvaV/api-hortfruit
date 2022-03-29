dd768efb2fe9517e38d62308002edcfa56e493b7
LEONARDO SILVA VICENTE <leonardo.svicente2@senacsp.edu.br>
2022-03-29 15:45:32 -0300

    atualizado

--- model/Constants.php
+++ model/Constants.php
@@ -1,7 +1,7 @@
 <?php
     define('DB_HOST','localhost');
-    define('DB_USER','root');
-    define('DB_PASS','');
+    define('DB_USER','usuario@leonardosv');
+    define('DB_PASS','104545Gta123');
     define('DB_NAME','hortfruitbd');
 
 ?>
