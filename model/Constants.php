8eeb55ab95af179e242ed6a2536fd35919f4a3ad
LEONARDO SILVA VICENTE <leonardo.svicente2@senacsp.edu.br>
2022-03-29 15:55:06 -0300

    final

--- model/Constants.php
+++ model/Constants.php
@@ -1,5 +1,5 @@
 <?php
-    define('DB_HOST','localhost');
+    define('DB_HOST','leonardosv.mysql.database.azure.com');
     define('DB_USER','usuario@leonardosv');
     define('DB_PASS','104545Gta123');
     define('DB_NAME','hortfruitbd');
